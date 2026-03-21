7577bbe12def98ca6ce3a6e65209235b6d7c870e
kregter <pihteevn@ru-drive.com>
2026-03-21 14:19:14 +0300

    add homerowk 7.2.1

---
+++ homework7.2/homework7.2.1/homework7.2.1.cpp
@@ -0,0 +1,81 @@
+﻿#include <string>
+#include <iostream>
+
+class Text {
+public:
+    virtual void render(const std::string& data) const {
+        std::cout << data;
+    }
+};
+
+class DecoratedText : public Text {
+public:
+    explicit DecoratedText(Text* text) : text_(text) {}
+    Text* text_;
+};
+
+class ItalicText : public DecoratedText {
+public:
+    explicit ItalicText(Text* text) : DecoratedText(text) {}
+    void render(const std::string& data)  const {
+        std::cout << "<i>";
+        text_->render(data);
+        std::cout << "</i>";
+    }
+};
+
+class BoldText : public DecoratedText {
+public:
+    explicit BoldText(Text* text) : DecoratedText(text) {}
+    void render(const std::string& data) const {
+        std::cout << "<b>";
+        text_->render(data);
+        std::cout << "</b>";
+    }
+};
+
+class Paragraph : public DecoratedText {
+public:
+    explicit Paragraph(Text* text) : DecoratedText(text) {}
+    void render(const std::string& data) const {
+        std::cout << "<p>";
+        text_->render(data);
+        std::cout << "</p>";
+    }
+};
+
+class Reversed : public DecoratedText {
+public:
+    explicit Reversed(Text* text) : DecoratedText(text) {}
+    void render(const std::string& data) const {
+        std::string data_in = data;
+        std::reverse(data_in.begin(), data_in.end());
+        text_->render(data_in);
+    }
+};
+
+class Link : public DecoratedText {
+public:
+    explicit Link(Text* text) : DecoratedText(text) {}
+    void render(const std::string& data1, const std::string& data2) const {
+        std::cout << "<a href=";
+        text_->render(data1);
+        std::cout << ">";
+        text_->render(data2);
+        std::cout << "</a>";
+    }
+};
+
+int main() {
+    auto text_block1 = new Paragraph(new Text());
+    text_block1->render("Hello world");
+    std::cout << std::endl;
+
+    auto text_block2 = new Reversed(new Text());
+    text_block2->render("Hello world");
+    std::cout << std::endl;
+
+    auto text_block3 = new Link(new Text());
+    text_block3->render("netology.ru", "Hello world");
+    std::cout << std::endl;
+}
